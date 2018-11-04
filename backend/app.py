5b072eda29701bcfc31863f1cdc28d233e7c10da
Cristian Plop <cristian.plop@tum.de>
2018-11-04 02:47:40 +0100

    changes

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,16 @@
 from flask import Flask, request
+from telethon import TelegramClient, events, sync, utils
+from telethon import functions, types
+from telethon.tl.functions.messages import GetDialogsRequest
+from telethon.tl.types import InputPeerEmpty
+from telethon.tl.types import PeerUser, PeerChat, PeerChannel, InputPeerUser, InputPeerChat, InputPeerChannel
+from telethon.tl.types.messages import Messages
 import json
 from slacker import Slacker
 import os
 from pymongo import MongoClient
+import asyncio
+from datetime import timezone
 
 app = Flask(__name__)
 slack = Slacker(os.getenv('SLACK_KEY'))
@@ -11,9 +19,67 @@ client = MongoClient(conn_string)
 db = client.get_database()
 messages_db = db['messages']
 
+api_id = 632827
+api_hash = '2bf8105b9536dbb821d61d00c621c16f'
+
+phone = '004915234723254'
+username = 'whoiscris'
+
+client = TelegramClient(username, api_id, api_hash)
+client.connect()
+
+# Ensure you're authorized
+if not client.is_user_authorized():
+    client.send_code_request(phone)
+    try:
+        client.sign_in(phone, input('Enter the code: '))
+    except SessionPasswordNeededError:
+        client.sign_in(password=input('Password: '))
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/telegram-hose', methods=['POST'])
+def get_telegram_msgs():
+    global api_id
+
+    get_dialogs = GetDialogsRequest(
+        offset_date=None, # datetime.datetime(2018, 10, 4),
+        offset_id=0,
+        offset_peer=InputPeerEmpty(),
+        limit=100,
+        hash=api_id,
+    )
+    dialogs = client(get_dialogs)
+
+    for u in dialogs.users:
+        if u.username is not None:
+            for message in client.iter_messages(u.username, limit=1000):
+                payload = {}
+                sender_first_name = message.sender.first_name if message.sender.first_name else ""
+                sender_last_name = message.sender.last_name if message.sender.last_name else ""
+                sender_username = message.sender.username if message.sender.username else ""
+                print(
+                        message.date, ":",
+                        sender_first_name, sender_last_name, sender_username,
+                        message.message,
+                        )
+
+                msg = message.message
+                user_id = message.from_id
+                user_name = sender_first_name + ',' + sender_last_name
+                timestamp = int(message.date.replace(tzinfo=timezone.utc).timestamp())
+                channel_id = message.id
+                payload['message'] = msg
+                payload['timestamp'] = timestamp
+                payload['user'] = user_name
+                payload['type'] = 'telegram'
+                print(payload)
+                result = messages_db.insert_one(payload)
+    return 'Ok'
+    
+
 
 @app.route('/slack-hose', methods=['POST'])
 def get_slack_msgs():
